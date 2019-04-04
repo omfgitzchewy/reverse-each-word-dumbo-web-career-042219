2342d8a2e31de42a01a1ffb737ef3df50e89dde9
Aaron Chu <aaronchu357@gmail.com>
2019-04-04 22:35:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
-  arr = sentence.split(" ")
-  arr.collect {|word| word.reverse}
-  return arr.join(" ")
+  sentence.collect {|word| word.reverse}
+  return sentence.join(" ")
 end
